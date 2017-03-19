1a98f026e160b9b73d460576febf74f950d66695
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2017-03-19 18:38:42 +0100

    refactor: remove obsolete func remove()

--- flac-mower.go
+++ flac-mower.go
@@ -92,12 +92,3 @@ func keys(s map[string]bool) []string {
 	}
 	return keys
 }
-
-func remove(s []string, r string) []string {
-	for i, v := range s {
-		if v == r {
-			return append(s[:i], s[i+1:]...)
-		}
-	}
-	return s
-}
